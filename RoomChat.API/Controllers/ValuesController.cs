d0a27cd1683ade92a4fff46275f0657cba63fdf6
Illia Hladush <illia.gladush@gmail.com>
2020-02-01 20:02:40 -0500

    Added client login and register

--- RoomChat.API/Controllers/ValuesController.cs
+++ RoomChat.API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace RoomChat.API.Controllers
             _context = context;
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues() //make method asynchronous
         {
